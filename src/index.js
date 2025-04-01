d4d6eaceec75db828c46b5c4cfb7425af1bad3f7
Stelly1506 <stelly.wu@edu.ece.fr>
2025-04-01 11:58:24 +0200

    f

--- src/index.js
+++ src/index.js
@@ -1,48 +1,38 @@
 import express from "express";
 import cors from "cors";
-import fs from 'fs';
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import pokemonRoutes from "./routes/pokemonRoutes.js";
 
+// Configuration des variables d'environnement
 dotenv.config();
 
-// Lire le fichier JSON
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const pokemonsList = JSON.parse(fs.readFileSync(path.join(__dirname, './data/pokemons.json'), 'utf8'));
+// Connexion à MongoDB
+connectDB();
 
-// Création de l'application Express
+// Configuration d'Express
 const app = express();
-const PORT = 3000;
-
-// Middleware pour CORS
-app.use(cors());
+const PORT = process.env.PORT || 3000;
 
 // Middleware pour parser le JSON
 app.use(express.json());
 
-// Middleware pour servir des fichiers statiques
-// 'app.use' est utilisé pour ajouter un middleware à notre application Express
-// '/assets' est le chemin virtuel où les fichiers seront accessibles
-// 'express.static' est un middleware qui sert des fichiers statiques
-// 'path.join(__dirname, '../assets')' construit le chemin absolu vers le dossier 'assets'
-app.use("/assets", express.static(path.join(__dirname, "../assets")));
+// Configuration CORS
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-// Route GET de base
-// req : requête de l'utilisateur
-// res : réponse du serveur, répondre à l'utilisateur
-app.get("/api/pokemons", (req, res) => {
-  res.status(200).send({
-    pokemons: pokemonsList,
-  });
-});
+// Routes
+app.use("/api/pokemons", pokemonRoutes);
 
+// Route de base
 app.get("/", (req, res) => {
-  res.send("bienvenue sur l'API Pokémon");
+  res.send("Bienvenue sur l'API Pokémon avec MongoDB");
 });
 
 // Démarrage du serveur
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serveur démarré sur http://0.0.0.0:${PORT}`);
 });
